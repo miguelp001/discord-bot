8ce7b01f64552fca9c3db0b711e5070e5b39ec29
miguelp001 <miguelp001@gmail.com>
2019-12-13 21:13:26 +0000

    fixed wordsalad and generation

--- cogs/toys.py
+++ cogs/toys.py
@@ -1,47 +1,11 @@
 from discord.ext import commands
-import jsonpickle, os, sys, random
-
-#class WordSalad(commands.Cog):
-#    def __init__(self, bot):
-#        self.bot = bot
-#
-#    def crazyClause(arg):
-#        # load wordSalad
-#        with open('wordsalad.json', 'r') as words:
-#            wordSalad = jsonpickle.decode(words.read())
-#
-#        # Decide how many clauses
-#        option = random.choice([True, False])
-#        
-#        # Generate the crazy
-#        if option == True:
-#            clause = random.choice(wordSalad['subject']) + ' ' + random.choice(wordSalad['predicate'])
-#            sentence = clause.capitalize() + random.choice(wordSalad['punctuation'])
-#            return sentence
-#        else:
-#            clause = random.choice(wordSalad['subject']) + ' ' +random.choice(wordSalad['predicate']) + ' ' + random.choice(wordSalad['conjunction']) + ' ' + random.choice(wordSalad['subject']) + ' ' + random.choice(wordSalad['predicate'])
-#            sentence = clause.capitalize() + random.choice(wordSalad['punctuation'])
-#            return sentence
-
-#    @commands.command()
-#    async def saycrazy(self, ctx):
-#        """Generate some crazytalk."""
-#        crazy = self.crazyClause()
-#        await ctx.send(crazy)
-#    
-#    @commands.command()
-#    async def talkcrazy(self, ctx):
-#        """Generate some crazytalk."""
-#        crazy = self.crazyClause()
-#        for i in range(2, 9):
-#            crazy = crazy + " " + self.crazyClause()
-#        await ctx.send(crazy)
+import jsonpickle, os, sys, random, discord
 
 class WordSalad(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    def randomClause(subject):
+    def randomClause(self, subject):
         # load wordSalad
         fileName = subject + '.json'
         with open(fileName, 'r') as words:
@@ -52,19 +16,24 @@ class WordSalad(commands.Cog):
         
         # Generate the crazy
         if option == True:
-            clause = random.choice(wordSalad['subject']) + ' ' + random.choice(wordSalad['predicate'])
+            clause = random.choice(wordSalad['descriptor']) + ' ' + random.choice(wordSalad['subject']) + ' ' + random.choice(wordSalad['action'])
+            
             sentence = clause.capitalize() + random.choice(wordSalad['punctuation'])
             return sentence
         else:
-            clause = random.choice(wordSalad['subject']) + ' ' +random.choice(wordSalad['predicate']) + ' ' + random.choice(wordSalad['conjunction']) + ' ' + random.choice(wordSalad['subject']) + ' ' + random.choice(wordSalad['predicate'])
+            clause = random.choice(wordSalad['descriptor']) + ' ' + random.choice(wordSalad['subject']) + ' ' + random.choice(wordSalad['action']) + ' ' + random.choice(wordSalad['conjunction']) + ' ' + random.choice(wordSalad['action'])
+            
             sentence = clause.capitalize() + random.choice(wordSalad['punctuation'])
             return sentence
 
     @commands.command()
     async def engram(self, ctx):
         """Generate an engram."""
-        engram = self.randomClause(destiny)
-        await ctx.send(engram)
+        subject = "destiny"
+        embed = discord.Embed(title="You open the engram to recieve:", colour=discord.Colour(0x4a90e2), description=self.randomClause(subject))
+        embed.set_thumbnail(url="https://cdn.discordapp.com/embed/avatars/0.png")
+
+        await ctx.send(embed=embed)
 
     @commands.command()
     async def saycrazy(self, ctx):
